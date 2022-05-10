d7141fd3f3d418b1b7b0ab083e37fa6a3b2f805e
Dindinha <indynha.w@gmail.com>
2022-05-10 15:03:57 -0300

    Validador de CPF

--- ProjetoJS/script.js
+++ ProjetoJS/script.js
@@ -4,23 +4,23 @@ function validaCPF(cpf) {
     if (cpf.length != 11) {
         return false;
     } else {
-        var numeros = cpf.substring(0, 9);
-        var digitos = cpf.substring(9);
+        let numeros = cpf.substring(0, 9);
+        let digitos = cpf.substring(9);
         /* console.log('Números do CPF ' + numeros); */
         /* console.log('Dígitos do CPF ' + digitos); */
 
-        var soma = 0;
+        let soma = 0;
         for(let i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i; // charAt retorna a posição daquela string na lista
         }
         console.log(soma);
 
         // Validação do primeiro dígito
-        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+        let resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
 
         if(resultado != digitos.charAt(0)) {
             return false;
-        } 
+        }
         /* console.log(digitos.toString().charAt(0) + ' é a primeira posição da variável soma'); */
 
 
@@ -37,14 +37,14 @@ function validaCPF(cpf) {
 }
 
 function validacao() {
-    var cpf = document.getElementById('cpf_digitado').value;
+    let cpf = document.getElementById('cpf_digitado').value;
      /* console.log(cpf); */
 
     document.getElementById('success').style.display = 'none';
     document.getElementById('error').style.display = 'none';
 
 
-    var resultadoValidacao = validaCPF(cpf);
+    let resultadoValidacao = validaCPF(cpf);
 
     if (resultadoValidacao) {
         document.getElementById('success').style.display = 'block';
